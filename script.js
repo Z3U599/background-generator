cc911a77d8d0f45b60cd0356442d79d0c3adfe20
unknown <alielyas778@gmail.com>
2021-11-16 23:13:54 +0430

    added more curse

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+
+
+
 function changeColor() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";";
+    css.textContent = body.style.background + "lol dumbass;";
 }
+
+
+
 
 color1.addEventListener("input", changeColor);
 color2.addEventListener("input", changeColor);
